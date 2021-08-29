66e1b96894ca7cfb99fdda35da78756419dee783
yash gupta <yash3144@gmail.com>
2021-08-29 17:53:16 +0530

    Add path

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const db= require('./db');
 const userRoutes = require('./routes/userRoute')
-// const path = require('path')
+const path = require('path')
 
 const app = express();
 app.use(express.json());
 
-// app.use('/api/pizzas',pizzaRoutes);
+
 app.use('/api/users',userRoutes);
-// app.use('/api/orders',orderRoutes);
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use('/',express.static('client/build'))
 
-//     app.get('*' , (req,res) => {
+if(process.env.NODE_ENV === 'production'){
+    app.use('/',express.static('client/build'))
 
-//         res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
-//     })
-// }
+    app.get('*' , (req,res) => {
+
+        res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
+    })
+}
 
 app.get('/',(req,res)=> {
     res.send("Server Working 🔥");
